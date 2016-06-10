52724c309db198126d586d62a6ff2fd9841fbd5a
Allen Boynton <aboynton@fullsail.edu>
2016-06-10 17:24:53 -0400

    Completed

---
+++ CE06/CE06/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CE06
+//
+//  Created by Allen Boynton on 6/10/16.
+//  Copyright © 2016 Full Sail. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
